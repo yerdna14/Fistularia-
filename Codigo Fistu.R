62cb7f39f02859c7700d69d83cf34b23e82abfa9
Maria Castillo <castillo.maria@correounivalle.edu.co>
2021-03-18 21:28:48 -0500

    Hola aquii

--- Codigo Fistu.R
+++ Codigo Fistu.R
@@ -9,28 +9,29 @@ aleja <- read.csv("Matriz corregida.csv")
 str(aleja)
 aleja$Intensidad <- as.factor(aleja$Intensidad)
 aleja$Coloracion <- as.factor(aleja$Coloracion)
+aleja$Actividad <- factor(aleja$Actividad,levels = c("Busqueda", "Alerta",  "Ataque"))
 
 ggplot(data = aleja, mapping = aes(x = Actividad, fill = Intensidad)) +
   geom_bar(alpha = .45, position = "fill") +
-   labs(y = "Proporción") + scale_fill_grey() + theme_classic() #Muy bacano
+   labs(y = "Proporción") + scale_fill_manual(values=c("grey78","grey53","dodgerblue4")) + theme_classic()#Muy bacano
 
 ggplot(data = aleja, mapping = aes(x = Ubicacion, fill = Coloracion)) +
   geom_bar(alpha = .45, position = "fill", width = 0.7) +
-  labs(y = "Proporción", fill = "Patron de Bandas") + scale_fill_grey() + theme_classic() #Muy bacano
+  labs(y = "Proporción", fill = "Patron de Bandas") + scale_fill_manual(values = c("grey78","dodgerblue4")) + theme_classic() #Muy bacano
 
 ggplot(data = aleja, mapping = aes(x = Actividad, fill = Coloracion)) +
   geom_bar(alpha = .45, position = "fill") +
-  labs(y = "Proporción", fill = "Patron de Bandas") + scale_fill_grey() + theme_classic() #Muy bacano
+  labs(y = "Proporción", fill = "Patron de Bandas") + scale_fill_manual(values = c("grey78","dodgerblue4"))+ theme_classic() #Muy bacano
 
-
+#Graficas con barras separadas 
 ggplot(data = aleja, mapping = aes(x = Actividad, fill = Intensidad)) +
   geom_bar(alpha = .45, position = "dodge") +
-  labs(y = "Proporción") + scale_fill_grey() + theme_classic() #Muy bacano
+  labs(y = "Proporción") + scale_fill_manual(values=c("grey78","grey53","dodgerblue4")) + theme_classic() #Muy bacano
 
 ggplot(data = aleja, mapping = aes(x = Ubicacion, fill = Coloracion)) +
   geom_bar(alpha = .45, position = "dodge", width = 0.7) +
-  labs(y = "Proporción", fill = "Patron de Bandas") + scale_fill_grey() + theme_classic() #Muy bacano
+  labs(y = "Proporción", fill = "Patron de Bandas") + scale_fill_manual(values = c("grey78","dodgerblue4")) + theme_classic() #Muy bacano
 
 ggplot(data = aleja, mapping = aes(x = Actividad, fill = Coloracion)) +
   geom_bar(alpha = .45, position = "dodge") +
-  labs(y = "Proporción", fill = "Patron de Bandas") + scale_fill_grey() + theme_classic() #Muy bacano
+  labs(y = "Proporción", fill = "Patron de Bandas") + scale_fill_manual(values = c("grey78","dodgerblue4")) + theme_classic() #Muy bacano
